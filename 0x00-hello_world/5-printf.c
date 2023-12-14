1f60419e803c0220ea0ae3121b541dde0bf79e62
SondosEssam <sondosessam713@gmail.com>
2023-12-13 23:16:36 -0800

    uploading

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main -Entry point of the program
+ *
+ * Description: This function prints a message using puts
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
