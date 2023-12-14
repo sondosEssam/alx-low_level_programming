99fbbeeaf68f846e19dce1c581e5caed941f29aa
SondosEssam <sondosessam713@gmail.com>
2023-12-13 23:32:09 -0800

    uploading

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main -Entry point of the program
+ *
+ * Description: This function prints a message using puts
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1:wq);
+}
