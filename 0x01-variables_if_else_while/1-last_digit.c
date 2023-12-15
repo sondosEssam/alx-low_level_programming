82494bb664a764ef798401efb71666502f91e10c
SondosEssam <sondosessam713@gmail.com>
2023-12-15 14:56:12 -0800

    uploading

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,8 @@
 /**
  * main -Entry point of the program
  *
- * Description: This program retrn if last digit is less than 0 , less than 5 or 6and more
+ * Description: This program retrn if last digit is less than 0
+ * less than 5 or 6and more
  * Return: Always 0 (success)
  */
 #include <stdlib.h>
@@ -14,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	s = n%10;
+	s = n % 10;
 
 	if (s > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, s);
