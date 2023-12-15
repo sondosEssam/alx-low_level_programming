41d78c2285963596d3688b978d5f1ca121d84298
SondosEssam <sondosessam713@gmail.com>
2023-12-15 15:04:25 -0800

    uploading

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main -Entry point of the program
+ *
+ * Description: prints all alphabets
+ * Return: Always 0 (success)
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
