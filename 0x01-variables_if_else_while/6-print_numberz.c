a50c7aed726a68c33426a30315a5c248de4f7307
SondosEssam <sondosessam713@gmail.com>
2023-12-16 08:17:01 -0800

    uploading

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,9 @@
 #include <string.h>
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
