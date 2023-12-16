a0f5e70e23b7461a6d2a27a1795fbfaefb39c1d4
SondosEssam <sondosessam713@gmail.com>
2023-12-16 09:34:40 -0800

    uploading

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+/**
+ * main -Entry point of the program
+ *
+ * Description: prints all alphabets upper and lower V2
+ * Return: Always 0 (success)
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	int c;
+	int j;
+	int i;
+	int z;
+	int r;
+
+	r = 1;
+	for (z = '0'; z <= '9'; z++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (c = z; c <= '9'; c++)
+			{
+				for (j = i + 1; j <= '9'; j++)
+				{
+					putchar(z);
+					putchar(i);
+					putchar(' ');
+					putchar(c);
+					putchar(j);
+					if (z == '9' && i == '8')
+					{
+						r = 0;
+					}
+					if (r == 1)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
