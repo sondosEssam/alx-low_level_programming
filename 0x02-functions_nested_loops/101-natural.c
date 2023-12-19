3aaea473d2bb38c26473bf61727c77ad620d45d8
SondosEssam <sondosessam713@gmail.com>
2023-12-19 11:45:43 -0800

    uploading

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+/**
+ * main - check the code
+ * Description: natural is natural
+ * Return: Always 0.
+ */
+#include<stdio.h>
+int main(void)
+{
+	int num;
+	long long sum;
+
+	sum = 0;
+	for (num = 3; num < 1024 ; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+			sum += num;
+	}
+	printf("%lld\n", sum);
+	return (0);
+}
