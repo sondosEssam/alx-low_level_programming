372d99fbd8246262b4cd8d41619f73daf3c5cf2d
SondosEssam <sondosessam713@gmail.com>
2023-12-19 12:31:48 -0800

    uploading

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+/**
+ * main -Entry point
+ *
+ * Description: prints all alphabet
+ * Return: 0
+ */
+#include"main.h"
+#include<stdio.h>
+int main(void)
+{
+	long prev;
+	long curr;
+	int c;
+	long next;
+	long sum;
+
+	prev = 1;
+	curr = 2;
+	sum = 0;
+	for (c = 1; c <= 48; c++)
+	{
+		next = curr + prev;
+		if (next % 2 == 0)
+			sum += next;
+		prev = curr;
+		curr = next;
+		if (curr >= 4000000)
+			break;
+
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+/**
+ * main -Entry point
+ *
+ * Description: prints all alphabet
+ * Return: 0
+ */
+#include"main.h"
+#include<stdio.h>
+int main(void)
+{
+	long prev;
+	long curr;
+	int c;
+	long next;
+
+	prev = 1;
+	curr = 2;
+	printf("%ld", prev);
+	printf(", %ld", curr);
+	for (c = 1; c <= 96; c++)
+	{
+		next = curr + prev;
+		printf(", %ld", next);
+		prev = curr;
+		curr = next;
+	}
+	putchar('\n');
+	return (0);
+}
