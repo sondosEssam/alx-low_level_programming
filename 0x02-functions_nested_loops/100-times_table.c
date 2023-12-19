3cec5c79fdb1865d34144cebe85c49c51fb4e876
SondosEssam <sondosessam713@gmail.com>
2023-12-19 11:28:24 -0800

    uploading

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-				else
+				else if (k >= 100)
 				{
 
 					_putchar(',');
@@ -50,4 +50,6 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
+	else
+		return;
 }
