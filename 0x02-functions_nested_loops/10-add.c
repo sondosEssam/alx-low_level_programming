41941f0bcc887c98ecfc0ffe5eeab26cec32e312
SondosEssam <sondosessam713@gmail.com>
2023-12-19 09:50:18 -0800

    uploading

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add -Entry point
+ * @a: -first arg
+ * @b: -second arg
+ * Description: add two num
+ * Return: Always 0
+ */
+#include"main.h"
+int add(int a, int b)
+{
+	return (a + b);
+}
