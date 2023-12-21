17179fbc0f26de771999c6aa02a6b659745cbeee
SondosEssam <sondosessam713@gmail.com>
2023-12-21 12:25:50 -0800

    uploading

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 /**
  * print_numbers -Entry point
  *
- * Description: check if uper or no
+ * Description: print nums from 0 to 9 
  * Return: 0
  */
 #include"main.h"
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c);
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+/**
+ * print_line -Entry point
+ * @n : num1
+ * Description: check if uper or no
+ * Return: 0
+ */
+#include"main.h"
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
