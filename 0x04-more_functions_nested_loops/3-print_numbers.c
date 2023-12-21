ba7c38b6842774cad70204dc3c2906d04364afa2
SondosEssam <sondosessam713@gmail.com>
2023-12-21 11:56:14 -0800

    uploading

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/**
+ * print_numbers -Entry point
+ *
+ * Description: check if uper or no
+ * Return: 0
+ */
+#include"main.h"
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
