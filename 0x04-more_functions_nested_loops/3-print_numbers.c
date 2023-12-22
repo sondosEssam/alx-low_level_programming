2f8bd341131e850df3f8121c0d4818295bf98fa5
SondosEssam <sondosessam713@gmail.com>
2023-12-21 22:13:26 -0800

    uploading

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 /**
  * print_numbers -Entry point
  *
- * Description: print nums from 0 to 9 
+ * Description: print nums from 0 to 9
  * Return: 0
  */
 #include"main.h"
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = 0; c <= 9; c++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(c + '0');
+		_putchar(i + 48);
 	}
 	_putchar('\n');
 }
