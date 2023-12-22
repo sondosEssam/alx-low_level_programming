1453ae7a3ae29b5e396e63bebcbca713b5a3ad89
SondosEssam <sondosessam713@gmail.com>
2023-12-21 22:37:26 -0800

    uploading

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+/**
+ * more_numbers -Entry point
+ *
+ * Description: print nums from 0 to 9
+ * Return: 0
+ */
+#include"main.h"
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int s1;
+	int s2;
+
+	s2 = 1;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <=14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(s2 + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+	_putchar('\n');
+	}
+}
