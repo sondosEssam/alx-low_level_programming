083c513498164b855077dc1f21805aa91395f402
SondosEssam <sondosessam713@gmail.com>
2023-12-22 00:08:08 -0800

    uploading

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+/**
+ * print_square -Entry point
+ * @size : size of square
+ * Description: check if uper or no
+ * Return: void
+ */
+#include"main.h"
+void print_square(int size)
+{
+	int i;
+	int l;
+
+	i =0;
+	l = size;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (size > 0)
+	{
+		while (i < l)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		size--;
+		i = 0;
+	}
+}
