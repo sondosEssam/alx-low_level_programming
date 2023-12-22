4aa311fe7e4b2d7c2b17966d57dd6f372e012c54
SondosEssam <sondosessam713@gmail.com>
2023-12-21 22:18:29 -0800

    uploading

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - print most numbrs
+ *
+ * Description: print nums from 0 to 9
+ * Return: void
+ */
+#include"main.h"
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
