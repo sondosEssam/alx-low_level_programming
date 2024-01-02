99494ffb281ec51198100b919de72b06d6219286
SondosEssam <sondosessam713@gmail.com>
2024-01-02 08:07:25 -0800

    uploading

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 -Entry point
+ *
+ * Description: this function makes the integer equal to 98
+ * @n: integer of this function
+ * Return : (void)
+ */
+#include"main.h"
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
