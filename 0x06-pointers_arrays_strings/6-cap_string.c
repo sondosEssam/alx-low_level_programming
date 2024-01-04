77bdc20f7dea7dcdacc96cead5be242953347aae
SondosEssam <sondosessam713@gmail.com>
2024-01-04 12:33:39 -0800

    uploading

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,26 @@
 /**
-* string_toupper -Function that concancate two strings
+* cap_string -Function that concancate two strings
 * @n: second strin
 * Return: pointer to the concanetated word
 */
 #include"main.h"
-char *string_toupper(char *n)
+char *cap_string(char *n)
 {
 	int tmp = 0;
+	int val = 0;
+	char chars [] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t' };
 
 	while (n[tmp] != '\0')
 	{
-	if (n[tmp] >= 97 && n[tmp] <= 122 && ((n[tmp-1]==','|| n[tmp-1]=';' || n[tmp-1] == '.' ||
-	n[tmp-1] == '!' || n[tmp-1] == '?' || n[tmp-1] == '"' || n[tmp-1]== '(' ||n[tmp-1]==')' ||n[tmp-1]== '{' and })
-		n[tmp] -= 32;
+		for (val = 0; val < 13; val++)
+		{
+			if (chars[val] == n[tmp] && n[tmp + 1] != '\0' && n[tmp + 1] >= 97)
+			{
+				n[tmp + 1] -= 32;
+				tmp++;
+				break;
+			}
+		}
 	tmp++;
 	}
 	return (n);
