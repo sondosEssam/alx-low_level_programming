ab37c1d721f471e4d0b53db0981fd914869b2401
SondosEssam <sondosessam713@gmail.com>
2024-01-04 09:35:52 -0800

    uploading

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int tmp = 0;
-	int i = 0;
 
 	if (s1[0] < s2[0])
 		tmp = *s2 - *s1 - '\0';
 	else if (s2[0] < s1[0])
 		tmp = *s2 - *s1 - '\0';
-	else
-	{
-		while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
-			i++;
-		if (s1[i] < s2[i])
-			tmp = *s2 - *s1 - '\0';
-		else if (s2[i] < s1[i])
-			tmp = *s1 - *s2 - '\0';
-	}
 	return (tmp);
 }
