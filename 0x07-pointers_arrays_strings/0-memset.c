9caac26763e779af2a816ff89bb598b2060bb27c
SondosEssam <sondosessam713@gmail.com>
2024-01-08 23:26:29 +0200

    uploading

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 /**
- * _memcpy - Function to do tehe for loop job
- * @dest: the dewsniation
- * @src: the source
+ * _memset - Function to do tehe for loop job
+ * @s: the dewsniation
+ * @b: the source
  * @n: no of bytes
  * Return: void
 */
 #include"main.h"
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	char *tmp = dest;
+	char *tmp = s;
 
 	while (n--)
 	{
-		*tmp = *src;
-		src++;
+		*tmp = b;
+		tmp++;
 	}
-	return (dest);
+	return (s);
 }
