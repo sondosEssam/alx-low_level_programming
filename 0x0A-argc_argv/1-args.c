4183abae82464c23989125cd25c61a6c57bfd0c2
SondosEssam <sondosessam713@gmail.com>
2024-01-12 23:43:57 +0200

    uploading

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+/**
+* main - entry point
+* @argc: no of args
+* @argv: args contents
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
