29b418f938bd2a88d97e21f111ba0dfa5857694e
SondosEssam <sondosessam713@gmail.com>
2024-01-12 22:50:21 +0200

    uploading

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,12 +1,22 @@
 /**
-* _strcpy -Function that concancate two strings
-* @dest: second string
-* @src: first string
-* Return: pointer to the concanetated word
-*/
+ * _strcpy -Entry point
+ *
+ * Description: this function output the  string
+ * @dest: the string of dest
+ * @src: source string
+ * Return: address of dest
+ */
 #include"main.h"
 char *_strcpy(char *dest, char *src)
 {
+	char *tmp = dest;
 
+	while (*src != 0)
+	{
+		*tmp = *src;
+		tmp++;
+		src++;
+	}
+	*tmp = *src;
 	return (dest);
 }
