a6bf41017430d6de83773c42ae070e90597d58eb
SondosEssam <sondosessam713@gmail.com>
2024-01-13 00:22:42 +0200

    uploading

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	{
 		if (*argv[i] == '0')
 			continue;
-		else if (atoi(argv[i]) == 0)
+		else if (*argv[i] < '0' || *argv[i] > '9')
 		{
 			printf("Error\n");
 			return (1);
