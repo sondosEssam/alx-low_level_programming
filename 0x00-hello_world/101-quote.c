9c144fe1d81c35af5de859ce1a3a2ca951eb64fc
SondosEssam <sondosessam713@gmail.com>
2024-01-14 22:01:10 +0200

    anything

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
