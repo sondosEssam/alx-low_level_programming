dc824d4fedd32e68773193e35caca3bdd4ea0011
SondosEssam <sondosessam713@gmail.com>
2024-01-16 12:37:31 +0200

    uploading

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ * create_array - function to create an array
+ * @size: size of the array
+ * @c: chars inside the array
+ * Return: pointeer
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	char *tmp;
+
+	if (size == 0)
+		return ('\0');
+	ptr = malloc(sizeof(int) * size);
+	tmp = ptr;
+	while (size--)
+	{
+		*tmp = c;
+		tmp++;
+	}
+	return (ptr);
+}
