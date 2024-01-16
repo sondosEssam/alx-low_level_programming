9d3a13c6c351cf26ce49244861194e5c9b739dc6
SondosEssam <sondosessam713@gmail.com>
2024-01-16 16:32:58 +0200

    uploading

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,14 @@ int **alloc_grid(int width, int height)
 	{
 		ptr[i] = malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+				free(ptr);
+			}
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
