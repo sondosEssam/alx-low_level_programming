58186005d449cbb51d6d21c64771cb10c0a75b0c
SondosEssam <sondosessam713@gmail.com>
2024-01-16 14:32:24 +0200

    uploading

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,19 +10,27 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 	int i = 0;
+	int j = 0;
 
-	if (str == NULL)
-		return ('\0');
-	while (str[i] != '\0')
+
+	while (s1[i] != '\0')
 		i++;
-	ptr = malloc((i));
+	while (s2[i] != '\0')
+		i++;
+	ptr = malloc(sizeof(char) * (i));
 	if (ptr == NULL)
 		return ('\0');
 	i = 0;
-	while (str[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		ptr[i] = str[i];
+		ptr[i] = s1[i];
 		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
 	}
 	return (ptr);
 }
--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,30 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * _strdup - function to create an array
- * @str: size of the array
- * Return: pointeer
-*/
-char *_strdup(char *str)
-{
-	char *ptr;
-	int i =0;
-
-	if (str == NULL)
-		return ('\0');
-	while (str[i] != '\0')
-		i++;
-	ptr = malloc(i + 1);
-	if (ptr == NULL)
-		return ('\0');
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ptr[i] = str[i];
-		i++;
-	}
-	return (ptr);
-}
 
 /**
  * main - check the code for ALX School students.
@@ -35,10 +11,10 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("");
+    s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
     printf("%s\n", s);
