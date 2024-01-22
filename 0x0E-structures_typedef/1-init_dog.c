704b4463062d04c90fe881c51b09fabbc0787238
SondosEssam <sondosessam713@gmail.com>
2024-01-22 23:26:59 +0200

    uploading

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include"dog.h"
+#include <stdlib.h>
+/**
+* init_dog - struct
+* @d: strcut
+* @name: name
+* @age: age
+* @owner: owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	(*d).owner = owner;
+
+}
