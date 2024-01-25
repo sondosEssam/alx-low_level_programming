63b335c7afbfe517f1e60d2036dd3e9b707a5302
SondosEssam <sondosessam713@gmail.com>
2024-01-25 20:50:09 +0200

    uploading

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include"function_pointers.h"
+/**
+ * array_iterator - fucntion
+ * @array: arg1
+ * @size: arg2
+ * @action: arg3
+ * Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
