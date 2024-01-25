3b8db558a080026419668567f43ed5dee1fb3aad
SondosEssam <sondosessam713@gmail.com>
2024-01-25 18:26:15 +0200

    uploading

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include"function_pointers.h"
+/**
+ * print_name - fucntion
+ * @name: arg1
+ * @f: arg2
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef START_H
+#define START_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif /* START_H */
