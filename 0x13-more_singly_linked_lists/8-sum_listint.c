37a40f59f04ee43ba9f8767efdbac6d2addee860
SondosEssam <sondosessam713@gmail.com>
2024-02-16 19:33:25 +0200

    sumOfAllElemnts

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * sum_listint - function to print lists
+ * @head: head of the list
+ * Return: no of nodes
+*/
+int sum_listint(listint_t *head);
+{
+	const listint_t *new_node;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	new_node = head;
+	while (new_node != NULL)
+	{
+		sum += new_node->n;
+		new_node = new_node->next;
+	}
+	return (sum);
+}
