d17565c9c9ded6a5c61bb9c6aeddb8f6ed46d890
SondosEssam <sondosessam713@gmail.com>
2024-02-16 21:32:34 +0200

    insetingwhereulike

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+#include<string.h>
+/**
+ * listint_t *insert_nodeint_at_index - function to print lists
+ * @head: head of the list
+ * @idx: no
+ * @n: char to be added
+ * Return: no of nodes
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *curr = *head, *next = (*head)->next;
+	int ni = 0;
+
+	if (*head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	while (curr->next != NULL)
+	{
+		if (ni == idx)
+		{
+			curr->next = new_node;
+			new_node->next = next;
+		}
+		n++;
+		curr = curr->next;
+		next = curr->next;
+	}
+	free(new_node);
+	return (NULL);
+}
