b26f8c4d850017f3c48418dac186814141513cad
SondosEssam <sondosessam713@gmail.com>
2024-02-16 17:33:38 +0200

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+#include<string.h>
+/**
+ * free_list - function to print lists
+ * @head: head of the list
+ * Return: no of nodes
+*/
+void free_list(list_t *head)
+{
+	list_t *prev = head;
+
+	head = head->next;
+	while (head->next != NULL)
+		{
+			free(prev);
+			prev = head;
+			head = head->next;
+		}
+		free(head);
+}
+
+
+
