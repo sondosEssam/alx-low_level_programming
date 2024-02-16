8bacb3d5d3746b8a7d82fca96adde3a00ed62b1b
SondosEssam <sondosessam713@gmail.com>
2024-02-16 19:06:50 +0200

    free v.2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+#include<string.h>
+/**
+ * free_listint2 - function to print lists
+ * @head: head of the list
+ * Return: no of nodes
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *curr = *head;
+
+	while (curr != NULL)
+	{
+		tmp = curr->next;
+		free(curr);
+		curr = tmp;
+	}
+	*head = NULL;
+}
