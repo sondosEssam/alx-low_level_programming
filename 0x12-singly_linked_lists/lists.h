e5860c71a6d35c8cf9760131eb94c5b6bdea5341
SondosEssam <sondosessam713@gmail.com>
2024-02-16 15:47:51 +0200

    this project header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,10 +10,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /* START_H */
