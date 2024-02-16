91f84b4fe6f47087f629f1428e8f79a214677ef3
SondosEssam <sondosessam713@gmail.com>
2024-02-16 21:33:15 +0200

    insetingwhereulike

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *curr = *head, *next = (*head)->next;
-	int ni = 0;
+	unsigned int ni = 0;
 
 	if (*head == NULL)
 		return (NULL);
