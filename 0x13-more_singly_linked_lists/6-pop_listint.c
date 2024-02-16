de0ea63894be3de1491702db6e1cb40f8655673e
SondosEssam <sondosessam713@gmail.com>
2024-02-16 19:14:16 +0200

    popElemnts

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+#include<string.h>
+/**
+ * pop_listint - function to print lists
+ * @head: head of the list
+ * Return: no of nodes
+*/
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *curr = *head;
+
+	n = curr->n;
+	curr = curr->next;
+	return (n);
+}
