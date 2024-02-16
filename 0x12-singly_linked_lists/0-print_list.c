c916155cab7da12605a4cd3f1785efb93aead6e1
SondosEssam <sondosessam713@gmail.com>
2024-02-16 15:31:14 +0200

    displaying linked lsit elemnt

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_list: function to print lists
+ * @h: head of the list
+ * Return: no of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *new_node = h;
+	size_t n = 0;
+
+	while (new_node != NULL)
+	{
+		if (new_node->str != NULL)
+			printf("[%d] %s\n", new_node->len, new_node->str);
+		else
+			printf("[0] (nil)\n");
+		n++;
+		new_node = new_node->next;
+	}
+	return (n);
+}
