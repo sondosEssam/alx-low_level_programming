f0ee1fbe9a2b8359a187a467be0987a622b9662a
SondosEssam <sondosessam713@gmail.com>
2024-02-17 10:39:23 +0200

    insertingAsYouLike

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,9 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_nodeint(head, n));
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next =  *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (curr->next != NULL)
 	{
 		if (ni == idx)
