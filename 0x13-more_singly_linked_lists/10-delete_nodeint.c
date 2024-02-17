1901b0a3c85550a964d99c7a0387cbcab0348614
SondosEssam <sondosessam713@gmail.com>
2024-02-17 11:53:53 +0200

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,14 +13,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *curr = *head, *prev = NULL, *next = NULL;
 	unsigned int ni = 0;
 
+	if (*head == NULL || head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = next;
 		free(curr);
 		return (1);
 	}
-	if (*head == NULL || head == NULL)
-		return (-1);
+
 	while (curr != NULL)
 	{
 		if (ni == index)
