754b374cf424fd5a27998ac35585adb06e44719b
SondosEssam <sondosessam713@gmail.com>
2024-03-05 22:58:34 +0200

    last task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,58 +1,61 @@
-#include"variadic_functions.h"
+#include "variadic_functions.h"
 /**
  * print_all - function
  * @format: arg
  * @...: arg3
  * Return: ivoid
-*/
-void print_all(const char * const format, ...)
+ */
+void print_all(const char *const format, ...)
 {
 	va_list args;
 	int i = 0, num = 0;
 	char *string = NULL;
 	double floati;
 	int flag = 0;
-	const unsigned int n = strlen(format);
 
+	if (format == NULL)
+		{
+			printf("\n");
+			return;
+		}
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
 		flag = 0;
-		switch(format[i])
+		switch (format[i])
 		{
-			case ('c'):
-			{
-				num = va_arg(args, int);
-				printf("%c", num);
-				break;
-			}
-			case ('i'):
-			{
-				num = va_arg(args, int);
-				printf("%d", num);
-				break;
-			}
-			case ('f'):
-			{
-				floati = va_arg(args, double);
-				printf("%f", floati);
-				break;
-			}
-			case ('s'):
-			{
-				string = va_arg(args, char*);
-				if (string == NULL)
-					string = "(nil)";
-				printf("%s", string);
-				break;
-			}
-			default:
-			{
+		case ('c'):
+		{
+			num = va_arg(args, int);
+			printf("%c", num);
+			break;
+		}
+		case ('i'):
+		{
+			num = va_arg(args, int);
+			printf("%d", num);
+			break;
+		}
+		case ('f'):
+		{
+			floati = va_arg(args, double);
+			printf("%f", floati);
+			break;
+		}
+		case ('s'):
+		{
+			string = va_arg(args, char *);
+				string == NULL ? string =  "(nil)" : string;
+			printf("%s", string);
+			break;
+		}
+		default:
+		{
 			flag = 1;
 			break;
-			}
-		} 
-		if( i != ((int)n - 1) && flag != 1)
+		}
+		}
+		if (i != (int)(strlen(format) - 1) && flag != 1)
 			printf(", ");
 		i++;
 	}
