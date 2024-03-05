f103db6f1255bd8e8d6f9a70331cb5c113a29f0f
SondosEssam <sondosessam713@gmail.com>
2024-03-05 22:44:40 +0200

    new header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,11 @@
 #ifndef START_H
 #define START_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif /* STRAT_H */
