6fc08291b17ff950646f59c9a9322479127bd8ad
SondosEssam <sondosessam713@gmail.com>
2024-03-21 18:05:52 +0200

    .h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -8,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
@@ -17,4 +17,5 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 #endif /* MAIN_H */
